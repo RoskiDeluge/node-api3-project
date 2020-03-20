f262ac770a43c62bd1f9925d030054fe53b2695e
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2020-03-19 20:52:33 -1000

    Finished coding .get .post .put .delete in userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -33,29 +33,83 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 router.get('/', (req, res) => {
   // do your magic!
   Users.get(req.query)
-    .then()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error retrieving the users."})
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  res.status(200).json(req.user)
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error retrieving posts for the user."})
+    })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  Users.remove(req.params.id)
+    .then(user => {
+      if (user > 0) {
+        res.status(200).json({ message: "User has been deleted." })
+      } else {
+        res.status(404).json({ message: "User could not be found."})
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error deleting user from database." })
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // do your magic!
+  Users.update(req.params.id, req.body)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "User could not be found."})
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error retrieving user from database."})
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  Users.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        next(new Error("invalid user id"))
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "exception", error})
+    })
+
 }
 
 function validateUser(req, res, next) {
